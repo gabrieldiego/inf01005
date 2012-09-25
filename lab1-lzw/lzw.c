4befac2c4b19275bd4959262f23777c6e3cbe3bd
Gabriel Teixeira <gabrieldiegoteixeira@gmail.com>
2012-09-25 17:56:34 -0300

    Output function separated as a callback

--- lab1-lzw/lzw.c
+++ lab1-lzw/lzw.c
@@ -1,8 +1,13 @@
 /* Grupo 3: Rafael / Gabriel Teixeira */
 #include "lzw.h"
 
-void init_lzw_encoder(lzw_enc_t *encoder_ctx, char first_char) {
+void init_lzw_encoder(
+  lzw_enc_t *encoder_ctx,
+  char first_char,
+  void (*output_func)(uint16_t)
+) {
   encoder_ctx->current_prefix = first_char;
+  encoder_ctx->output_func = output_func;
 }
 
 void insert_char(lzw_enc_t *encoder_ctx, dictionary_t *dict, char input_char) {
@@ -21,6 +26,10 @@ void insert_char(lzw_enc_t *encoder_ctx, dictionary_t *dict, char input_char) {
 	printf("Output codeword: 0x%04X\n",encoder_ctx->current_prefix);
 	encoder_ctx->current_prefix = input_char;
   }
+}
+
+void output_last_codeword(lzw_enc_t *encoder_ctx) {
+  printf("Output codeword: 0x%04X\n",encoder_ctx->current_prefix);
 }
 
 /* Clear dictionary once it is full */
