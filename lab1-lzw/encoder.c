c7f7694b64b48943e29a019393cd6803d5fa1f2c
Gabriel Teixeira <gabrieldiegoteixeira@gmail.com>
2012-10-02 19:40:19 -0300

    Added the ASCII file format according to the spec.

--- lab1-lzw/encoder.c
+++ lab1-lzw/encoder.c
@@ -2,6 +2,8 @@
 #include <stdio.h>
 #include "lzw.h"
 
+#define ENCODE_IN_ASCII
+
 void print_output_codeword(uint16_t codeword) {
   printf("Output codeword: 0x%04X\n",codeword);
 }
@@ -54,14 +56,80 @@ void write_to_file(uint16_t codeword, void *ptr) {
   }
 }
 
+typedef struct write_to_file_ascii_s {
+  char *output_file_name;
+  FILE *output_file;
+  uint8_t bits_encoded;
+} write_to_file_ascii_t;
+
+int init_write_to_file_ascii(write_to_file_ascii_t *ctx, char *output_file_name) {
+  uint8_t group_number=3; /* Group 3 */
+
+  ctx->output_file_name = output_file_name;
+  ctx->output_file = fopen(ctx->output_file_name,"wb");
+  if(!ctx->output_file) {
+	fprintf(stderr,"Error while opening %s\n",ctx->output_file_name);
+	return -1;
+  }
+
+  fputc(group_number, ctx->output_file);
+  /*This should be the number of bits encoded, but this only will be known at
+	the end of the file encoding */
+  fputc(group_number, ctx->output_file);
+
+  ctx->bits_encoded = 0;
+  return 0;
+
+}
+
+void write_to_file_ascii(uint16_t codeword, void *ptr) {
+  write_to_file_ascii_t *ctx = (write_to_file_ascii_t *)ptr;
+  int i;
+
+  if(ctx->bits_encoded > ctx->bits_encoded+12) {
+	fprintf(stderr,"Number of bits encoded overflows.\n");
+	fprintf(stderr,"Files can't have more than 256 bits according to spec.\n");
+
+	/* Finish writing the file anyways */
+	codeword = 0xFFFF;
+  } else {
+	/* If no overflow is expected, increase the number of bits */
+	ctx->bits_encoded += 12;
+  }
+
+  if(codeword == 0xFFFF) {
+	/* This codeword is to close the output file */
+	
+	/* Keep in mind that this overflows after 256 */
+	fputc('a', ctx->output_file);
+
+	fclose(ctx->output_file);
+	return;
+  }
+
+  /* Bit banging MSB */
+  for(i=0;i<12;i++) {
+	if(codeword&0x400) {
+	  fputc('1', ctx->output_file);
+	} else {
+	  fputc('0', ctx->output_file);
+	}
+	codeword <<=1; /* Shift the data to get the next bit */
+  }
+
+}
+
 int main(int argc, char **argv) {
   FILE *input_file;
   dictionary_t dict;
   lzw_enc_t encoder;
   char char_input;
   char output_file_name[]="output.dout";
+#ifdef ENCODE_IN_ASCII
+  write_to_file_ascii_t write_to_file_ascii_ctx;
+#else /* ENCODE_IN_ASCII */
   write_to_file_t write_to_file_ctx;
-  
+#endif /* ENCODE_IN_ASCII */
 
   if(argc < 2) {
     printf("Usage.\n");
@@ -74,10 +142,15 @@ int main(int argc, char **argv) {
     printf("Error opening input file %s.\n",argv[1]);
   }
 
+#ifdef ENCODE_IN_ASCII
+  if(init_write_to_file_ascii(&write_to_file_ascii_ctx, output_file_name)) {
+	return -1;
+  }
+#else /* ENCODE_IN_ASCII */
   if(init_write_to_file(&write_to_file_ctx, output_file_name)) {
 	return -1;
   }
-
+#endif /* ENCODE_IN_ASCII */
 
   if(argc >= 3) {
 	init_dict(&dict,argv[2]);
@@ -87,7 +160,11 @@ int main(int argc, char **argv) {
 
   char_input = fgetc(input_file);
   if(char_input != EOF) {
+#ifdef ENCODE_IN_ASCII
+	init_lzw_encoder(&encoder,char_input,write_to_file_ascii,&write_to_file_ascii_ctx);
+#else /* ENCODE_IN_ASCII */
 	init_lzw_encoder(&encoder,char_input,write_to_file,&write_to_file_ctx);
+#endif /* ENCODE_IN_ASCII */
   }
 
   char_input = fgetc(input_file);
@@ -99,6 +176,13 @@ int main(int argc, char **argv) {
 
   output_last_codeword(&encoder);
 
+#ifdef ENCODE_IN_ASCII
+  write_to_file(0xFFFF,&write_to_file_ascii_ctx);
+#else /* ENCODE_IN_ASCII */
   write_to_file(0xFFFF,&write_to_file_ctx);
+#endif /* ENCODE_IN_ASCII */
+
+  //write_dict_to_file(stdout, &dict);
+
   return 0;
 }
