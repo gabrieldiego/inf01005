068c87f3aaa95631a91a6588937e81db26cb4086
Rafael Machado <rafakatarn@gmail.com>
2012-10-02 21:29:26 -0300

    Fix da implemeção dos itens 2 e 3 do relatório de saída

--- lab1-lzw/decoder/report.c
+++ lab1-lzw/decoder/report.c
@@ -64,13 +64,13 @@ void report_generate(char *file_in, char *file_out)
     entropia=0;
     entropia=(-log2(cont_0/(cont_0+cont_1))*cont_0)+(-log2(cont_1/(cont_0+cont_1))*cont_1);
     
-    printf("Entropia do alfabeto: %.3f \n",entropia);
+    fprintf(report_file, "Entropia do alfabeto: %.3f \n\n",entropia);
     
 
     // item 3) a quantidade média de informação recebida por símbolo da mensagem (nesse caso, a mensagem é todo o arquivo de entrada)
     q_media=entropia/(cont_0+cont_1);
     
-    printf("Quantidade média de informação recebida por símbolo: %.3f \n",q_media);
+    fprintf(report_file, "Quantidade média de informação recebida por símbolo: %.3f \n\n",q_media);
 
     // item 4) o percentual de compactação do arquivo codificado de entrada em relação ao original
 
